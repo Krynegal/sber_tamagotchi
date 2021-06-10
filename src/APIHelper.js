31611cba4322a1474856d253b91b6ef0b7948a38
Анна Плохотнюк <annaplokhotnyuk@MacBook-Pro-Anna-2.local>
2021-06-10 16:26:22 +0300

    a

--- src/APIHelper.js
+++ src/APIHelper.js
@@ -1,19 +1,11 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/";
-<<<<<<< HEAD
-//const API_URL = "https://serene-inlet-82302.herokuapp.com/";
+//const API_URL = "http://127.0.0.1:8000/";
+const API_URL = "https://serene-inlet-82302.herokuapp.com/";
 
 export async function createUser(UserId, name, foodLevel, sleepLevel, playLevel, timeOfExit, sec, min, hour, kusua, flag) {
   const { data: answer } = await axios.post(API_URL + "user/", {
     UserId: UserId,
-=======
-
-export async function createUser(UserId, SberId, name, foodLevel, sleepLevel, playLevel, timeOfExit, sec, min, hour) {
-  const { data: answer } = await axios.post(API_URL + "user/", {
-    UserId: UserId,
-    SberId: SberId,
->>>>>>> 2cece1dcef35d671738c5e1dc354a21b0c525ee1
     name: name,
     foodLevel: foodLevel,
     sleepLevel: sleepLevel,
@@ -22,11 +14,8 @@ export async function createUser(UserId, SberId, name, foodLevel, sleepLevel, pl
     sec: sec,
     min: min,
     hour: hour,
-<<<<<<< HEAD
     kusua: kusua,
     flag: flag,
-=======
->>>>>>> 2cece1dcef35d671738c5e1dc354a21b0c525ee1
   });
   return answer;
 }
@@ -40,7 +29,6 @@ export async function getUser(UserId) {
   return answer;
 }
 
-<<<<<<< HEAD
 export async function updateUser(UserId,
   name,
   foodLevel,
@@ -68,23 +56,6 @@ export async function updateUser(UserId,
     console.log('body:', body);
   await axios.put(API_URL + "user/", {
     UserId: UserId,
-=======
-//обновление времени выхода
-export async function updateUser(UserId, name, foodLevel, playLevel, sleepLevel, timeOfExit, sec, min, hour) {
-  const body = {
-      UserId,
-      name,
-      foodLevel,
-      playLevel, 
-      sleepLevel, 
-      timeOfExit,
-      sec, 
-      min, 
-      hour,
-    };
-  console.log('body:', body);
-  await axios.put(API_URL + `user/${UserId}`, {
->>>>>>> 2cece1dcef35d671738c5e1dc354a21b0c525ee1
     name: name,
     foodLevel: foodLevel,
     playLevel: playLevel,
@@ -92,12 +63,8 @@ export async function updateUser(UserId, name, foodLevel, playLevel, sleepLevel,
     timeOfExit: timeOfExit,
     sec: sec,
     min: min,
-<<<<<<< HEAD
     hour: hour,
     kusua: kusua,
     flag: flag,
-=======
-    hour: hour
->>>>>>> 2cece1dcef35d671738c5e1dc354a21b0c525ee1
   });
 }
